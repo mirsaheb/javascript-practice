2db6e102c8781d99b0366715be9eaf576d798278
mirsaheb <masalammir@gmail.com>
2023-01-28 13:40:15 +0600

    end 17

--- js/script.js
+++ js/script.js
@@ -34,11 +34,45 @@ document.getElementById("output").innerHTML = x[3]+x[4]+" = total";
 
 var onion = 30;
 var garlic= 60;
+var rice = 55;
 
-// Change variable value
+// Change variable Update
 onion = 40;
+
+garlic = garlic+10;
+
+rice += 10;
 
  var Total = onion+garlic;
 
 //  document.getElementById ("output").innerHTML= "Total Price "+Total;
- document.getElementById ("output").innerHTML= "Total Price = "+(onion+garlic);
+ document.getElementById ("output-1").innerHTML= "Total Price = "+(onion+garlic+rice);
+
+ var first_name = "Justin";
+ var last_name = "Baber"
+
+ document.getElementById ("output-2").innerHTML= first_name+last_name;
+ document.getElementById ("output-3").innerHTML= first_name+' '+last_name;
+
+ var pant = 450;
+ var shirt = '650';
+
+ document.getElementById("output-4").innerHTML = pant+shirt;
+ 
+ shirt = parseInt(shirt);
+
+ document.getElementById("output-5").innerHTML = pant+shirt;
+
+ var shoe = "1999.99"
+
+ shoe = parseFloat(shoe);
+
+ document.getElementById("output-6").innerHTML = pant+shirt+shoe;
+
+
+ var isHappy = true;
+
+ var test = [10, true, 'Salam']
+
+ document.getElementById('output-7').innerHTML = typeof(isHappy);
+ document.getElementById('output-8').innerHTML = typeof(test);
