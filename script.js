a8cd6c3d5f3aafb400c0f8b2ae36b580d6cc67ae
mirsaheb <masalammir@gmail.com>
2023-01-28 11:20:05 +0600

    class 17-2

---
+++ script.js
@@ -0,0 +1,17 @@
+var a = 23;
+var b= 12;
+var c = 15;
+var d = 20
+let total = 100
+
+// document.getElementById("output").innerHTML = a+b+c+d;
+// document.getElementById("output").innerHTML = total-(a+b+c+d);
+
+// document.getElementById("output").innerHTML ='Total Taka Rest of the hand '+total-(a+b+c+d); Error 
+
+var x = [23, 35, 42, 37, 33, 38];
+
+document.getElementById("output").innerHTML = x[3]+x[4]+" = total";
+
+// document.getElementById("output").innerHTML = total-(a+b+c+d)+'Total Taka Rest of the hand '; OK 
+
