860b7a360cd52033cd6769c1ffc958015f8b48ca
mirsaheb <masalammir@gmail.com>
2023-01-28 12:13:59 +0600

    class 17-6

--- js/script.js
+++ js/script.js
@@ -1,8 +1,21 @@
+
+// Data type Numeric
 var a = 23;
 var b= 12;
 var c = 15;
 var d = 20
 let total = 100
+
+
+// Datatype String
+
+var name = "Mir Abdus Salam"
+
+// Data type Boolean
+
+var this_correct = true
+
+// https://www.w3schools.com/js/js_reserved.asp
 
 // document.getElementById("output").innerHTML = a+b+c+d;
 // document.getElementById("output").innerHTML = total-(a+b+c+d);
@@ -11,7 +24,21 @@ let total = 100
 
 var x = [23, 35, 42, 37, 33, 38];
 
+// Assignment Operator "="
+
 document.getElementById("output").innerHTML = x[3]+x[4]+" = total";
 
 // document.getElementById("output").innerHTML = total-(a+b+c+d)+'Total Taka Rest of the hand '; OK 
 
+// Operation 
+
+var onion = 30;
+var garlic= 60;
+
+// Change variable value
+onion = 40;
+
+ var Total = onion+garlic;
+
+//  document.getElementById ("output").innerHTML= "Total Price "+Total;
+ document.getElementById ("output").innerHTML= "Total Price = "+(onion+garlic);
